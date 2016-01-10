2e271bb1b02d6df2e2d2a0607a7d8020da91e832
Max Koreychenko <koreychenko@gmail.com>
2016-01-11 01:18:34 +0300

    fix tabs php warnings

--- elements_tabs/element-tabs-field-tabs.tpl.php
+++ elements_tabs/element-tabs-field-tabs.tpl.php
@@ -1,11 +1,12 @@
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <div class="field-items row"<?php print $content_attributes; ?>>
+    <div class="col-md-12">
     <div class="element-tabs">
       <ul>
       <?php foreach ($items as $delta => $item): ?>
         <?php if (isset($item['entity'])): ?>
           <?php $fields = array_pop($item['entity']['field_collection_item']); ?>
-          <?php $id = 'element-tabs-'.$fields['#entity_view_mode']['id'].'-'.$delta; ?>
+          <?php $id = 'element-tabs-'.$fields['#entity']->item_id.'-'.$delta; ?>
           <li><a href="#<?php print $id; ?>"><?php print $fields['field_tabs_title'][0]['#markup']; ?></a></li>
         <?php endif; ?>
       <?php endforeach; ?>
@@ -13,12 +14,13 @@
       <?php foreach ($items as $delta => $item): ?>
         <?php if (isset($item['entity'])): ?>
           <?php $fields = array_pop($item['entity']['field_collection_item']); ?>
-          <?php $id = 'element-tabs-'.$fields['#entity_view_mode']['id'].'-'.$delta; ?>
+          <?php $id = 'element-tabs-'.$fields['#entity']->item_id.'-'.$delta; ?>
           <div id="<?php print $id; ?>">
             <?php print $fields['field_tabs_text'][0]['#markup']; ?>
           </div>
         <?php endif; ?>
       <?php endforeach; ?>
+    </div>
     </div>
   </div>
 </div>
